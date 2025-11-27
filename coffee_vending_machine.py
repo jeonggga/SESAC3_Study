25744e5264f2a6a74b186ecc5d8aeedcde7a11ee
jeonggga <gyj_802@naver.com>
2025-11-27 17:55:40 +0900

    Revert "251127 데이터프레임 공부"
    This reverts commit 667524e7e040bb975f92c3ba0e6c717d37b4860a.

--- coffee_vending_machine.py
+++ coffee_vending_machine.py
@@ -2,14 +2,13 @@ recipe = {
     "americano":[("bean", 25), ("water", 8)],
     "cafelatte":[("bean", 25), ("milk", 8)],
     "matchalatte":[("matcha", 25), ("milk", 8)],
-    "chocolatte":[("choco", 25), ("milk", 8)]
+    "espresso":[("bean", 30)]
 }   # 음료별 레시피 딕셔너리
 
 
 ingredient = {
-    "bean": 100,
-    "matcha": 100,
-    "choco": 100,
+    "bean": 50,
+    "matcha": 50,
     "water": 100,
     "milk": 100
 }   # 재료별 초기 재고 딕셔너리
@@ -19,7 +18,7 @@ price = {
     "americano": 2000,
     "cafelatte": 3500,
     "matchalatte": 4500,
-    "chocolatte": 4500
+    "espresso": 2000
 }   # 음료별 가격 딕셔너리
 
 
@@ -27,7 +26,7 @@ question = """
 1. americano
 2. cafelatte
 3. matchalatte
-4. chocolatte
+4. espresso
 원하시는 음료의 번호를 골라주세요 : """ # 음료 선택 질문 문자열
 
 
@@ -47,7 +46,7 @@ def select_drink():
         key = "matchalatte"
         
     elif selected_num == 4:
-        key = "chocolatte"
+        key = "espresso"
         
     else:
         print("잘못된 입력입니다. 다시 입력해주세요.")
@@ -58,46 +57,94 @@ def select_drink():
     return key  # 선택한 음료 문자열, 정수 반환
 
 
-# def pay():
-
-
 
 
 # 음료 제조 함수
 def making_drink(key):
     print(f"{key} 제조가 완료되었습니다.")
+
+
+
+
+# 재고 확인 함수
+def ingredient_check(key):  # 선택된 음료 메뉴의 현재 재고를 확인함
+    # 현재 남은 재고를 여기 함수에서 사용할 변수에 할당함
+    used_bean = ingredient["bean"]
+    used_water = ingredient["water"]
+    used_milk = ingredient["milk"]
+    used_matcha = ingredient["matcha"]
     
-
-
-# 선택한 음료 메뉴의 현재 재고를 확인하는 함수
-def ingredient_check(key):
-    # recipe 딕셔너리의 (메뉴명, 재료목록) 쌍을 하나씩 꺼냄 / items()는 딕셔너리의 키와 값을 쌍으로 꺼내는 메소드
-    for key_menu, value_tuple in recipe.items():
-        # 인자값으로 받은 key(선택한 음료)가 recipe 딕셔너리의 key(key_menu)와 일치하면
-        if key == key_menu:
-            # recipe 딕셔너리의 값인 value_tuple(튜플)안에 있는 2개 요소를 반복문을 통해 가져옴
-            # ingr_name: 재료 이름, required_amount: 필요한 용량
-            for ingr_name, required_amount in value_tuple:
-                # 현재 재고가 필요한 용량(required_amount)보다 크거나 같다면
-                if ingredient[ingr_name] >= required_amount:
-                    # 현재 재고에서 필요한 용량을 빼서 현재 재고에 저장함
-                    ingredient[ingr_name] -= required_amount
-                    
-                    print("사용한 용량: ", ingr_name, required_amount)
-                    print("남은 용량: ", ingr_name, ingredient[ingr_name])
-                
-                else:   # 크거나 같지 않다면, 재고가 부족한 경우
-                    print("현재 원두 재고: ", ingredient["bean"])
-                    print("수량이 부족합니다. 다른 메뉴를 선택해주세요.")
-                    return select_drink()   # 음료 선택 함수로 돌아감
-                    
-            return making_drink(key)    # 모든 재료가 충분하다면 음료 제조 함수 호출
+    if key == "americano":  # 선택한 음료가 아메리카노라면
+        # (남은 원두 수가 필요한 원두 수보다 크거나 같다면) 그리고 (남은 뜨거운 물이 필요한 뜨거운 물보다 크거나 같다면)
+        if (used_bean >= recipe["americano"][0][1]) and (used_water >= recipe["americano"][1][1]):
+            # 남은 원두 수 = 현재 재고 원두 수 - 필요한 원두 수
+            used_bean = ingredient["bean"] - recipe["americano"][0][1]
+            # 남은 뜨거운 물의 양 = 현재 뜨거운 물의 양 - 필요한 뜨거운 물의 양
+            used_water = ingredient["water"] - recipe["americano"][1][1]
+            # 남은 원두 수를 현재 재고 원두 수에 할당함
+            ingredient["bean"] = used_bean
+            # 남은 뜨거운 물의 양을 현재 재고 남은 뜨거운 물의 양에 할당함
+            ingredient["water"] = used_water
+            
+            print("현재 원두 재고: ", ingredient["bean"])
+            print("현재 물의 양: ", ingredient["water"])
+            
+            # 재고 확인하고 재료 소진 후 음료 제작 함수로 이동
+            return making_drink(key)
         
-    # ingredient_check 함수 실행 (select_drink 함수의 결과를 인자로 사용)
+        else:
+            print("현재 원두 재고: ", ingredient["bean"])
+            print("수량이 부족합니다. 다른 메뉴를 선택해주세요.")
+            
+            return select_drink()
+        
+        
+    elif key == "cafelatte":
+        if used_bean >= recipe["cafelatte"][0][1] and used_milk >= recipe["cafelatte"][1][1]:
+            used_bean = ingredient["bean"] - recipe["cafelatte"][0][1]
+            used_milk = ingredient["milk"] - recipe["cafelatte"][1][1]
+            ingredient["bean"] = used_bean
+            ingredient["milk"] = used_milk
+            print(ingredient["bean"])
+            print(ingredient["milk"])
+            return making_drink(key)
+        
+        else:
+            print("현재 원두 재고: ", ingredient["bean"])
+            print("수량이 부족합니다. 다른 메뉴를 선택해주세요.")
+            return select_drink()
+        
+        
+    elif key == "matchalatte":
+        if used_bean >= recipe["matchalatte"][0][1] and used_milk >= recipe["matchalatte"][1][1]:
+            used_matcha = ingredient["matcha"] - recipe["matchalatte"][0][1]
+            used_milk = ingredient["milk"] - recipe["matchalatte"][1][1]
+            ingredient["matcha"] = used_matcha
+            ingredient["milk"] = used_milk
+            print(ingredient["matcha"])
+            print(ingredient["milk"])
+            return making_drink(key)
+        
+        else:
+            print("현재 말차 재고: ", ingredient["matcha"])
+            print("수량이 부족합니다. 다른 메뉴를 선택해주세요.")
+            return select_drink()
+        
+        
+    elif key == "espresso":
+        if used_bean >= recipe["espresso"][0][1]:
+            used_bean = ingredient["bean"] - recipe["espresso"][0][1]
+            ingredient["bean"] = used_bean
+            print(ingredient["bean"])
+            return making_drink(key)
+        
+        else:
+            print("현재 원두 재고: ", ingredient["bean"])
+            print("수량이 부족합니다. 다른 메뉴를 선택해주세요.")
+            return select_drink()
+        
+        
+ingredient_check(select_drink())
 
 
 
-def coffee_machine():
-    ingredient_check(select_drink())
-    
-coffee_machine()
